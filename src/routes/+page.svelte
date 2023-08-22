605d066902a04812b191314a84194f03d9637fc7
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-22 11:36:42 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-	var x: number = 0;
+  var x: number = 0
 </script>
 
 <h1>
-	SVS <button on:click={() => x++}>{x}</button>
+  SVS <button on:click={() => x++}>{x}</button>
 </h1>
 
 <style lang="scss">
-	$number_norm: rgb(45, 83, 101);
-	$number_hover: rgb(138, 90, 90);
-	$number_active: rgb(151, 67, 67);
-	h1 {
-		text-shadow: 1px 1px 3px black;
-		button {
-			all: unset;
-			display: inline-block;
-			color: $number_norm;
-			cursor: pointer;
-		}
-		button:hover {
-			color: $number_hover;
-		}
-		button:active {
-			color: $number_active;
-		}
-	}
+  $number_norm: rgb(45, 83, 101);
+  $number_hover: rgb(138, 90, 90);
+  $number_active: rgb(151, 67, 67);
+  h1 {
+    text-shadow: 1px 1px 3px black;
+    button {
+      all: unset;
+      display: inline-block;
+      color: $number_norm;
+      cursor: pointer;
+    }
+    button:hover {
+      color: $number_hover;
+    }
+    button:active {
+      color: $number_active;
+    }
+  }
 </style>
