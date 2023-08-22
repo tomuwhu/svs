5ad6d5c3c710379c3fcbead7c0fbea3331c6947c
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-22 16:42:16 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,89 @@
 <script lang="ts">
-	var x: number = 0;
+  import serverurl from './serverurl.js'
+  var ServerURL: string = serverurl
+  var un: string
+  var pw: string
+  $: loggedin = appdata.un && appdata.name
+  import { onMount } from 'svelte'
+  import axios from 'axios'
+  import md5 from 'md5'
+  interface Appdata {
+    un: string
+    name: string
+    err: unknown
+    msg: string
+  }
+  const appdata: Appdata = { un: '-', name: '...', err: null, msg: '' }
+  onMount(async () => {
+    appdata.un = localStorage.getItem('un') || ''
+    try {
+      const res = await axios.post(ServerURL, appdata.un)
+      appdata.un = res.data.un
+      appdata.name = res.data.name
+    } catch (e: unknown) {
+      appdata.err = e
+    }
+  })
+  function login(e: any) {
+    try {
+      const res = axios.post(ServerURL + 'login.php', { un, pw: md5(pw) }).then((res) => {
+        appdata.un = res.data.un
+        if (appdata.un == null) {
+          appdata.msg = 'Hibás felhasználónév vagy jelszó!'
+          setInterval(() => (appdata.msg = ''), 1000)
+        }
+        appdata.name = res.data.name
+        localStorage.setItem('un', appdata.un)
+      })
+    } catch (e: unknown) {
+      appdata.err = e
+    }
+  }
+  function logout(e: any) {
+    try {
+      const res = axios.post(ServerURL + 'logout.php', appdata.un).then((res) => {
+        appdata.un = ''
+        appdata.name = '...'
+        localStorage.removeItem('un')
+      })
+    } catch (e: unknown) {
+      appdata.err = e
+    }
+  }
 </script>
 
-<h1>
-	SVS <button on:click={() => x++}>{x}</button>
-</h1>
+<h1>SOB Intranet</h1>
+{#if !loggedin}
+  <input type="text" placeholder="Felhasználónév" bind:value={un} />
+  <br /><br />
+  <input type="password" placeholder="Jelszó" bind:value={pw} />
+  <br /><br />
+  {#if un && pw}
+    <button on:click={login}>Login</button>
+  {/if}
+  <br /><br />
+{:else if !appdata.err}
+  <div>{appdata.name}</div>
+  <br />
+  <br />
+  <button on:click={logout}>Logout</button>
+{/if}
+{#if appdata.err}
+  <span>{appdata.err}</span>
+  <br />
+{/if}
+<span>{appdata.msg}</span>
 
 <style lang="scss">
-	$number_norm: rgb(45, 83, 101);
-	$number_hover: rgb(138, 90, 90);
-	$number_active: rgb(151, 67, 67);
-	h1 {
-		text-shadow: 1px 1px 3px black;
-		button {
-			all: unset;
-			display: inline-block;
-			color: $number_norm;
-			cursor: pointer;
-		}
-		button:hover {
-			color: $number_hover;
-		}
-		button:active {
-			color: $number_active;
-		}
-	}
+  h1 {
+    text-shadow: 1px 1px 3px black;
+  }
+  span {
+    color: red;
+  }
+  div {
+    color: rgb(55, 70, 76);
+    font-size: 20px;
+    text-shadow: 1px 1px 3px gray;
+  }
 </style>
