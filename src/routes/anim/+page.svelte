ab0f975f07a79bcb1ea425983f065a53bd70d48e
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-23 23:25:15 +0200

    .

---
+++ src/routes/anim/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  var x = 0,
+    vx = 0
+  setInterval(() => (x += x >= -20 ? vx : 1), 10)
+</script>
+
+<h1>Animáció</h1>
+<button style="left: {x}px" on:click={() => vx = 0}/>
+<input type="range" bind:value={vx} min={-5} max={5} step={0.1} />
+
+<style>
+  button {
+    position: fixed;
+    top: 56px;
+    width: 20px;
+    height: 20px;
+    background-color: red;
+  }
+</style>
