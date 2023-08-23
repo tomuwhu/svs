e571890886681cf483c20ac8599585c83bd8d96f
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-23 12:49:19 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
