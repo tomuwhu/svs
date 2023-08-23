d550f66d7677c8c1f34a5b4f8509e13bea295d22
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-23 17:14:36 +0200

    .

---
+++ backend/req.php
@@ -0,0 +1,18 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    $mysqli = new mysqli("localhost","root","","test");
+    $mysqli -> set_charset("utf8");
+    $sql = "SELECT * FROM sob_users";
+    $line = [];
+    $s = "[";
+    if ($result = $mysqli->query($sql)) {
+        while($obj = $result->fetch_object()){
+            $s .= json_encode($obj).',';
+        }
+    }
+    echo rtrim($s,",")."]";
+    $result->close();
+    unset($obj);
+    unset($sql);
+?>
