e5ade86a7f070417816ad6b19f1da9831d29eee7
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-23 17:42:11 +0200

    .

---
+++ docs/_app/immutable/nodes/0.299ec845.js
@@ -0,0 +1,1 @@
+import{L as n}from"../chunks/layout.b72e5c7a.js";const e=!0,o=Object.freeze(Object.defineProperty({__proto__:null,prerender:e},Symbol.toStringTag,{value:"Module"}));export{n as component,o as universal};
