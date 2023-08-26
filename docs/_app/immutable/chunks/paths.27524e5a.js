eb1a6dac46d464f1be2ec02e0ad5a3cc1ee5d420
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-08-26 11:14:28 +0200

    .

--- docs/_app/immutable/chunks/paths.27524e5a.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1ml5z0d)==null?void 0:s.base)??"/svs";var a;const t=((a=globalThis.__sveltekit_1ml5z0d)==null?void 0:a.assets)??e;export{t as a,e as b};
--- docs/_app/immutable/chunks/serverurl.5c6be767.js
+++
@@ -1,1 +0,0 @@
-const e="https://inf.u-szeged.hu/~tnemeth/SOB/";export{e as s};
